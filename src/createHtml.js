063345b5e2e7eac39ddd817b1d2eade6e653f232
gtankha <46304828+gtankha@users.noreply.github.com>
2020-09-25 15:42:57 -0700

    finished html generator

--- src/createHtml.js
+++ src/createHtml.js
@@ -7,27 +7,32 @@ emplData.forEach(element => {
  if ((element.team_member) ==  'Engineer') { 
 
     
- returnText = returnText +  `   
-    <div class="card-body border border-dark m-4 p-2">
-       <div class="card-header bg-primary text-light border border-dark m-0"><b>${element.engineer_name}</b> </br> <b>Engineer</b> </div>
+ returnText = returnText +  ` 
+ <div class="card border-dark">  
+    <div class="card-body bg-light">
+       <div class="card-header bg-primary text-light"><b>${element.engineer_name}</b> </br> <b>Engineer</b> </div>
         <ul class="list-group list-group-flush bg-light m-6">
             <li class="list-group-item">ID: ${element.engineer_ID}</li>
             <li class="list-group-item">E-mail:  ${element.engineer_email} </li>
             <li class="list-group-item">gitHub: ${element.engineer_gitHub}</li>
         </ul>
-    </div>`
+    </div>
+</div>`
+
 }  
 if ((element.team_member) ==  'Intern') { 
     
-    returnText = returnText +  `   
-       <div class="card-body border border-dark m-4 p-2">
-          <div class="card-header bg-primary text-light border border-dark m-0"><b>${element.intern_name}</b> </br> <b>Intern</b> </div>
+    returnText = returnText +  `
+    <div class="card border-dark">   
+       <div class="card-body bg-light">
+          <div class="card-header bg-primary text-light"><b>${element.intern_name}</b> </br> <b>Intern</b> </div>
            <ul class="list-group list-group-flush bg-light m-6">
                <li class="list-group-item">ID: ${element.intern_ID}</li>
                <li class="list-group-item">E-mail:  ${element.intern_email} </li>
                <li class="list-group-item">School: ${element.intern_school}</li>
            </ul>
-       </div>`
+       </div>
+    </div>`
    }
 
 }
@@ -55,7 +60,7 @@ const createHtml = data => {
     <title>Team Profile Generator</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   </head>
 
   <body>
@@ -64,20 +69,30 @@ const createHtml = data => {
      </header>
 
      <main>
-        <div class="card" style="width: 18rem">
-            <div class="card-body border border-dark m-4 p-2">
-               <div class="card-header bg-primary text-light border border-dark m-0"><b>${manager.team_manager_name}</b> </br> <b>Manager</b> </div>
-                <ul class="list-group list-group-flush bg-light m-6">
+     <div class = "card-columns m-5">
+        
+     
+        <div class="card border-dark">
+            <div class="card-body bg-light">
+               <div class="card-header bg-primary text-light"><b>${manager.team_manager_name}</b> </br> <b>Manager</b> </div>
+                <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${manager.team_manager_employee_ID}</li>
                     <li class="list-group-item">E-mail:  ${manager.team_manager_email} </li>
                     <li class="list-group-item">Office Number: ${manager.team_manager_office_ID}</li>
                 </ul>
             </div>
         </div>
+    
+      
 
         ${generateEmpCard(team)}
 
-        </div>
+       
+
+    </div>
+    
+        
+   
 
      </main>
 
